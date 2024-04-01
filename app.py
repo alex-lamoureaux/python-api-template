adf6af3fd17e5607c4d400f556ae869571aead87
Alex Lamoureaux <github@lamoureaux.me>
2024-04-01 15:13:04 -0500

    add file loading sample code

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return '<h1>Hello, World!</h1>'
 
+@app.route('/users')
+def get_users():
+  with open('users.json') as f:
+    data = json.load(f)
+    return data
 
 if __name__ == "__main__":
   app.run(debug=True)
